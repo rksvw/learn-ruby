6477e34db9ca261145e6231a185b2870e189b516
rksvw <ritik.vw98@gmail.com>
2025-06-01 10:57:17 +0530

    i/o methods

---
+++ i_o.rb
@@ -0,0 +1,25 @@
+"""
+Ouput
+"""
+ew line after execution code
+print "Hello World"
+
+# Append new line after execution code
+puts "Hello World"
+
+
+"""
+Input
+"""
+
+name = gets
+p name
+
+
+def name_is()
+    puts "Hello"
+end
+
+name_is
+
+# Not append n
